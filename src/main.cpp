ea325fba5ee2e5e801f6a55089b81cd8f2662ced
nstcrystal <nstcrystal@gmail.com>
2026-07-30 15:03:15 +0700

    Refactor dashboard layout: adjust spacing and reintroduce About section for improved readability

--- src/main.cpp
+++ src/main.cpp
@@ -188,11 +188,6 @@ int main()
 
             // ===== Left Panel =====
             left.push_back(
-                text("  About")
-                | bold
-            );
-
-            left.push_back(
                 text("Profile")
                 | bold
                 | center
@@ -201,29 +196,34 @@ int main()
             left.push_back(separator());
 
             left.push_back(
+                text("  About")
+                | bold
+            );
+
+            left.push_back(
                 hbox({
-                    text("Username    : ") | bold,
+                    text("Username     : ") | bold,
                     text(userData["login"].get<std::string>())
                 })
             );
 
             left.push_back(
                 hbox({
-                    text("Followers   : ") | bold,
+                    text("Followers    : ") | bold,
                     text(std::to_string(userData["followers"].get<int>()))
                 })
             );
 
             left.push_back(
                 hbox({
-                    text("Public Repos: ") | bold,
+                    text("Public Repos : ") | bold,
                     text(std::to_string(userData["public_repos"].get<int>()))
                 })
             );
 
             left.push_back(
                 hbox({
-                    text("Total Stars : ") | bold,
+                    text("Total Stars  : ") | bold,
                     text(std::to_string(totalStars))
                 })
             );
@@ -237,13 +237,13 @@ int main()
             );
 
             left.push_back(
-                text("C++        :  " + std::to_string(cpp)));
+                text("C++          : " + std::to_string(cpp)));
 
             left.push_back(
-                text("Python     :  " + std::to_string(python)));
+                text("Python       : " + std::to_string(python)));
 
             left.push_back(
-                text("TypeScript :  " + std::to_string(ts))
+                text("TypeScript   : " + std::to_string(ts))
             );
 
             // ===== Right Panel =====
